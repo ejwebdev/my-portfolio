d0308c42fbbeb79f7c5132bbf318ece7aa5b7aad
ejwebdev <ejwebdev25@gmail.com>
2024-10-13 23:38:55 +0800

    Tailwind and React

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ReactGA from "react-ga4";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactGA.initialize("G-3NW9G16D1F");
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
