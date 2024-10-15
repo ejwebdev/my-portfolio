8c138d99096343bb3c57561723f66fde64fd90fa
ejwebdev <ejwebdev25@gmail.com>
2024-10-15 12:51:30 +0800

    Tailwind and React

--- src/components/projects-page/Projects.jsx
+++ src/components/projects-page/Projects.jsx
@@ -2,6 +2,7 @@ import project1 from "../../assets/project1.jpg";
 import project2 from "../../assets/project2.jpg";
 import project3 from "../../assets/project3.jpg";
 import project4 from "../../assets/project4.jpg";
+import project5 from "../../assets/project5.jpg";
 import "./projects.css";
 
 const projects = [
@@ -10,28 +11,35 @@ const projects = [
         link: "https://ejwebdev.github.io/meows-sweet-bites/",
         title: "Meow's Sweet Bites",
         desc: "A website for a local business cakery and flower shop, developed and designed in collaboration with the client, to showcase her products and special offers.",
-        tech: ["Tailwind", "React", "React-Router", "Node.js"],
+        tech: ["Tailwind", "React", "React-Router", "Node.js", "Git"],
     },
     {
         img: project2,
         link: "https://ejwebdev.github.io/greenvista/",
         title: "GreenVista",
         desc: "A real estate project website, developed and designed as a personal project to refine my web development skills and showcase my readiness for larger clients or companies.",
-        tech: ["Tailwind", "React", "React-Router", "Node.js"],
+        tech: ["Tailwind", "React", "React-Router", "Node.js", "Git"],
     },
     {
         img: project3,
         link: "https://ejwebdev.github.io/eventify/",
         title: "Eventify",
         desc: "An event management website, developed and designed as a personal project to demonstrate my flexibility in creating various types of websites.",
-        tech: ["Tailwind", "React", "React-Router", "Node.js"],
+        tech: ["Tailwind", "React", "React-Router", "Node.js", "Git"],
     },
     {
         img: project4,
+        link: "https://dribbble.com/shots/25030627-E-Commerce-Gymshark-Inspired-Project",
+        title: "Gymshark PH",
+        desc: "An e-commerce website inspired by the official Gymshark website, developed and designed using the Shopify platform as a personal project to showcase my Shopify development skills.",
+        tech: ["Shopify", "Liquid", "Custom CSS"],
+    },
+    {
+        img: project5,
         link: "https://ejwebdev.github.io/firebase-auth/",
         title: "Firebase Auth",
         desc: "An authentication form website inspired by Firebase, developed and designed as a personal project to showcase my backend database management skills for upcoming projects.",
-        tech: ["HTML", "Tailwind", "JavaScript", "Node.js", "Firebase"],
+        tech: ["HTML", "Tailwind", "JavaScript", "Node.js", "Firebase", "Git"],
     },
 ];
 
